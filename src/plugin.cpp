adc10a80f9f7d7865176da3d76af84ba8ed81887
B. W. Lewis <blewis@illposed.net>
2014-04-03 19:26:18 -0400

    Plugin.cpp now sets SciDB version number automatically

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -3,10 +3,6 @@
 * BEGIN_COPYRIGHT
 *
 * PARADIGM4 INC.
-* This file is part of the Paradigm4 Enterprise SciDB distribution kit
-* and may only be used with a valid Paradigm4 contract and in accord
-* with the terms and conditions specified by that contract.
-*
 * Copyright © 2010 - 2012 Paradigm4 Inc.
 * All Rights Reserved.
 *
@@ -24,17 +20,13 @@
 #include <vector>
 
 #include "SciDBAPI.h"
+#include "system/Constants.h"
 
 
-/**
- * EXPORT FUNCTIONS
- * Functions from this section will be used by LOAD LIBRARY operator.
- */
 EXPORTED_FUNCTION void GetPluginVersion(uint32_t& major, uint32_t& minor, uint32_t& patch, uint32_t& build)
 {
-    // Provide correct values here. SciDB check it and does not allow to load too new plugins.
-    major = 13;
-    minor = 6;
-    patch = 0;
-    build = 0;
+    major = scidb::SCIDB_VERSION_MAJOR();
+    minor = scidb::SCIDB_VERSION_MINOR();
+    patch = scidb::SCIDB_VERSION_PATCH();
+    build = scidb::SCIDB_VERSION_BUILD();
 }
