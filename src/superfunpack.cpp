eb24a652697a5283bb8a9562824138e45dc49b3d
bwlewis <blewis@illposed.net>
2014-05-13 16:44:11 -0400

    notes

--- src/superfunpack.cpp
+++ src/superfunpack.cpp
@@ -15,6 +15,28 @@
 *
 * END_COPYRIGHT
 */
+
+/*  The stats functions below are adapted from the R source code:
+ *
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
+ *  Copyright (C) 1997--2005  The R Core Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
+
 #define _XOPEN_SOURCE
 #include <stdio.h>
 #include <string.h>
@@ -151,6 +173,10 @@ pfconvert(const Value** args, Value *res, void*)
 
 /* ***************************************************************************
  *       Hypergeometric stuff in support of Fisher's exact test 
+ *
+ * Adapted from the R source code, Copyrigth 1998-2014 R Foundataion.
+ * The changes are mainly to use the boost distribution functions and the
+ * available boost root-finding method.
  * ***************************************************************************
  */
 class mnhyper
