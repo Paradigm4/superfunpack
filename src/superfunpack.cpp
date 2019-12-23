6641a68bb5a6ef54ebccc162859fbb342a6271d8
wenweiP4 <wxiong@paradigm4.com>
2019-12-23 15:54:42 +0000

    Change the murmur hash function from 32-bit to 64-bit

--- src/superfunpack.cpp
+++ src/superfunpack.cpp
@@ -56,6 +56,7 @@
 
 #include "pcrs.h"
 #include "R/fun.h"
+#include "MurmurHash3.h"
 
 using namespace std;
 using namespace scidb;
@@ -674,53 +675,6 @@ tm2s(const Value** args, Value *res, void*)
   res->setDouble(s);
 }
 
-#define ROT32(x, y) ((x << y) | (x >> (32 - y))) // avoid effort
-static uint32_t murmur3_32(char const* key, uint32_t len, uint32_t const seed = 0x5C1DB123)
-{
-    static const uint32_t c1 = 0xcc9e2d51;
-    static const uint32_t c2 = 0x1b873593;
-    static const uint32_t r1 = 15;
-    static const uint32_t r2 = 13;
-    static const uint32_t m = 5;
-    static const uint32_t n = 0xe6546b64;
-    uint32_t hash = seed;
-    const int nblocks = len / 4;
-    const uint32_t *blocks = (const uint32_t *) key;
-    int i;
-    uint32_t k;
-    for (i = 0; i < nblocks; i++)
-    {
-        k = blocks[i];
-        k *= c1;
-        k = ROT32(k, r1);
-        k *= c2;
-        hash ^= k;
-        hash = ROT32(hash, r2) * m + n;
-    }
-    const uint8_t *tail = (const uint8_t *) (key + nblocks * 4);
-    uint32_t k1 = 0;
-    switch (len & 3)
-    {
-    case 3:
-        k1 ^= tail[2] << 16;
-    case 2:
-        k1 ^= tail[1] << 8;
-    case 1:
-        k1 ^= tail[0];
-        k1 *= c1;
-        k1 = ROT32(k1, r1);
-        k1 *= c2;
-        hash ^= k1;
-    }
-    hash ^= len;
-    hash ^= (hash >> 16);
-    hash *= 0x85ebca6b;
-    hash ^= (hash >> 13);
-    hash *= 0xc2b2ae35;
-    hash ^= (hash >> 16);
-    return hash;
-}
-
 static void
 murmur_hash(const Value** args, Value *res, void*)
 {
@@ -730,7 +684,11 @@ murmur_hash(const Value** args, Value *res, void*)
     return;
   }
   const char * key = args[0]->getString();
-  int64_t value = murmur3_32(key, strlen(key));
+  // int64_t value = murmur3_32(key, strlen(key));
+  uint32_t const seed = 0x5C1DB123;
+  uint64_t value=0;
+  MurmurHash3_x64_128(key, strlen(key), seed, &value);
+   
   res->setInt64(value);
 }
 
